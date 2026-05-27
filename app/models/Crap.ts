64ed54dd267316a44e619b94ca390c6e6de55e81
Ingrid <ingridstanic@gmail.com>
2026-05-27 12:49:39 +0200

    fix wrong type to isImportant property

--- app/models/Crap.ts
+++ app/models/Crap.ts
@@ -3,7 +3,7 @@ import { model, Schema, models } from "mongoose";
 const crapToStoreSchema = new Schema({
   id: { type: String, required: true },
   text: { type: String, required: true },
-  isImportant: { type: String, required: true },
+  isImportant: { type: Boolean, required: true },
 });
 
 export const Crap = models.crap || model("crap", crapToStoreSchema);

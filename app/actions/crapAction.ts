de2be212c012401d06ecda6bed556fa9ee4a5d0f
Ingrid <ingridstanic@gmail.com>
2026-05-28 16:11:27 +0200

    implementet zod validation to input

--- app/actions/crapAction.ts
+++ app/actions/crapAction.ts
@@ -2,17 +2,30 @@
 import { connectDB } from "../lib/db";
 import { Crap } from "../models/Crap";
 import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+
+const crapSchema = z.object({
+  text: z
+    .string()
+    .trim()
+    .min(1, "Can not store empty input")
+    .max(20, "To much to store"),
+});
 
 export const storeCrap = async (formData: FormData) => {
   await connectDB();
 
-  const craptext = formData.get("craptext") as string;
+  const result = crapSchema.safeParse({ text: formData.get("craptext") });
 
-  //validering checka in zod
+  if (!result.success) {
+    console.log(result.error);
+  }
+
+  console.log("your crap is now stored!");
 
   await Crap.create({
     id: uuidv4(),
-    text: craptext,
+    text: result.data?.text,
     isImportant: false,
   });
 };
